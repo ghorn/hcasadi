d937baab80d83373e928226a9e6255da02452178
Greg Horn <gregmainland@gmail.com>
2011-07-27 16:19:49 -0700

    finished implementing q function quadratic expansion functions qx/qu/qxx/quu/qxu and updated test module spring.hs

--- Hom.hs
+++ Hom.hs
@@ -2,12 +2,13 @@
 
 {-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
 
-module Hom (q,State(..), Action(..), Ode(..), eulerStep, rk4Step, dA, dB, qx, qu, qxx, quu, qxu, Quad(..), evalQuad) where
+module Hom (State(..), Action(..), Ode(..), eulerStep, rk4Step, dA, dB,
+            cx, cu, cxx, cuu, cxu,
+            qx, qu, qxx, quu, qxu, Quad(..), evalQuad) where
 
 import Numeric.AD
 import Data.Traversable
 import Numeric.LinearAlgebra
-
 
 type State a = [a]
 type Action a = [a]
@@ -45,49 +46,79 @@ dB f x u = jacobian g u
     g u' = f (map lift x) u'
 
 
----- quadratic expansion of cost(x,u)
-qx :: Floating a => (forall s a. (Floating a, Mode s) =>
+---------- quadratic expansion of cost(x,u) ----------
+cx :: Floating a => (forall s a. (Floating a, Mode s) =>
                      [AD s a] -> [AD s a] -> AD s a) -> (State a -> Action a -> [a])
-qu :: Floating a => (forall s a. (Floating a, Mode s) =>
+cu :: Floating a => (forall s a. (Floating a, Mode s) =>
                      [AD s a] -> [AD s a] -> AD s a) -> (State a -> Action a -> [a])
-qxx :: Floating a => (forall s a. (Floating a, Mode s) =>
+cxx :: Floating a => (forall s a. (Floating a, Mode s) =>
                       [AD s a] -> [AD s a] -> AD s a) -> (State a -> Action a -> [[a]])
-quu :: Floating a => (forall s a. (Floating a, Mode s) =>
+cuu :: Floating a => (forall s a. (Floating a, Mode s) =>
                       [AD s a] -> [AD s a] -> AD s a) -> (State a -> Action a -> [[a]])
-qxu :: Floating a => (forall s a. (Floating a, Mode s) =>
+cxu :: Floating a => (forall s a. (Floating a, Mode s) =>
                       [AD s a] -> [AD s a] -> AD s a) -> (State a -> Action a -> [[a]])
 
 
-qx cost x u = grad g x
+cx cost x u = grad g x
   where
     g x' = cost x' (map lift u)
 
-qu cost x u = grad g u
+cu cost x u = grad g u
   where
     g u' = cost (map lift x) u'
 
-qxx cost x u = hessian g x
+cxx cost x u = hessian g x
   where
     g x' = cost x' (map lift u)
 
-quu cost x u = hessian g u
+cuu cost x u = hessian g u
   where
     g u' = cost (map lift x) u'
 
-qxu cost x u = jacobian g u
+cxu cost x u = jacobian g u
   where
-    g u' = (qx cost) (map lift x) u'
+    g u' = (cx cost) (map lift x) u'
 
 
-qx' :: forall a. Floating a =>
+-------- quadratic expansion of q function (unmaximized value function) -------
+qx :: forall a. Floating a =>
        (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> AD s a)
        -> (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> State (AD s a))
        -> State a
        -> Action a
        -> Quad a
        -> [a]
+qu :: forall a. Floating a =>
+       (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> AD s a)
+       -> (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> State (AD s a))
+       -> State a
+       -> Action a
+       -> Quad a
+       -> [a]
+qxx :: forall a. Floating a =>
+       (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> AD s a)
+       -> (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> State (AD s a))
+       -> State a
+       -> Action a
+       -> Quad a
+       -> [[a]]
+quu :: forall a. Floating a =>
+       (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> AD s a)
+       -> (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> State (AD s a))
+       -> State a
+       -> Action a
+       -> Quad a
+       -> [[a]]
 
-qx' cost dode x u (Quad vxx vx v0 x0) = grad g x
+qxu :: forall a. Floating a =>
+       (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> AD s a)
+       -> (forall s a. (Floating a, Mode s) => State (AD s a) -> Action (AD s a) -> State (AD s a))
+       -> State a
+       -> Action a
+       -> Quad a
+       -> [[a]]
+
+qx cost dode x u (Quad vxx vx v0 x0) = grad g x
   where
     g x' = q' x' (map lift u) (Quad vxx' vx' v0' x0')
       where
@@ -95,10 +126,54 @@ qx' cost dode x u (Quad vxx vx v0 x0) = grad g x
         vx' = map lift vx
         v0' = lift v0
         x0' = map lift x0
-    
+
     q' :: forall s. Mode s => State (AD s a) -> Action (AD s a) -> Quad (AD s a) -> AD s a
     q' = q cost dode
 
+qu cost dode x u (Quad vxx vx v0 x0) = grad g u
+  where
+    g u' = q' (map lift x) u' (Quad vxx' vx' v0' x0')
+      where
+        vxx' = map (map lift) vxx
+        vx' = map lift vx
+        v0' = lift v0
+        x0' = map lift x0
+
+    q' :: forall s. Mode s => State (AD s a) -> Action (AD s a) -> Quad (AD s a) -> AD s a
+    q' = q cost dode
+
+qxx cost dode x u (Quad vxx vx v0 x0) = hessian g x
+  where
+    g x' = q' x' (map lift u) (Quad vxx' vx' v0' x0')
+      where
+        vxx' = map (map lift) vxx
+        vx' = map lift vx
+        v0' = lift v0
+        x0' = map lift x0
+
+    q' :: forall s. Mode s => State (AD s a) -> Action (AD s a) -> Quad (AD s a) -> AD s a
+    q' = q cost dode
+
+quu cost dode x u (Quad vxx vx v0 x0) = hessian g u
+  where
+    g u' = q' (map lift x) u' (Quad vxx' vx' v0' x0')
+      where
+        vxx' = map (map lift) vxx
+        vx' = map lift vx
+        v0' = lift v0
+        x0' = map lift x0
+
+    q' :: forall s. Mode s => State (AD s a) -> Action (AD s a) -> Quad (AD s a) -> AD s a
+    q' = q cost dode
+
+qxu cost dode x u (Quad vxx vx v0 x0) = jacobian g u
+  where
+    g u' = (qx cost dode) (map lift x) u' (Quad vxx' vx' v0' x0')
+      where
+        vxx' = map (map lift) vxx
+        vx' = map lift vx
+        v0' = lift v0
+        x0' = map lift x0
 
 q :: Floating a => (State a -> Action a -> a) -> Ode a -> (State a -> Action a -> Quad a -> a)
 q cost dode = \x u v -> (cost x u) + (nextValue x u v)
@@ -114,8 +189,8 @@ evalQuad (Quad h g a x0) x = constTerm + linearTerm + quadraticTerm
      constTerm = a
      linearTerm = dotLists g x' -- put length check here
      quadraticTerm = (*0.5) $ dotLists x' $ map (\y -> dotLists y x') h -- put length check here
- 
+
      dotLists :: Floating a => [a] -> [a] -> a
      dotLists x0 x1 = sum (zipWith (*) x0 x1)
-     
+
      x' = zipWith (-) x x0
--- spring.hs
+++ spring.hs
@@ -1,8 +1,10 @@
+-- spring.hs
+
 module Main where
 
 import Hom
 
-
+-- spring ode
 sDxdt :: Floating a => Ode a
 sDxdt state action = [v, (-k*x - b*v + u)/mass] where
   x    = state  !! 0
@@ -12,23 +14,23 @@ sDxdt state action = [v, (-k*x - b*v + u)/mass] where
   b    = 0.1
   mass = 1
 
-
+-- euler step
 sEuler :: Floating a => State a -> Action a -> a -> State a
 sEuler x u dt = eulerStep sDxdt x u dt
 
-
+-- rk4 step
 sRk4 :: Floating a => State a -> Action a -> a -> State a
 sRk4 x u dt = rk4Step sDxdt x u dt
 
-
+-- dynamics linearizations dA/dB
+-- f ~= dA*(x-x0) + dB*(u-u0) + f0
 sA :: Floating a => State a -> Action a -> [[a]]
 sA = dA sDxdt
-
 
 sB :: Floating a => State a -> Action a -> [[a]]
 sB = dB sDxdt
 
-
+-- cost fcn to test following expansions
 sCost :: Floating a => State a -> Action a -> a
 sCost x u = 7*position*position + 2*velocity*velocity + 4*force*force
   where
@@ -36,16 +38,30 @@ sCost x u = 7*position*position + 2*velocity*velocity + 4*force*force
     velocity = x !! 1
     force = u !! 0
 
+-- cost fcn quadratic expansion
+sCx :: Floating a => State a -> Action a -> [a]
+sCu :: Floating a => State a -> Action a -> [a]
+sCxx :: Floating a => State a -> Action a -> [[a]]
+sCuu :: Floating a => State a -> Action a -> [[a]]
+sCxu :: Floating a => State a -> Action a -> [[a]]
 
-sQx :: Floating a => State a -> Action a -> [a]
-sQu :: Floating a => State a -> Action a -> [a]
-sQxx :: Floating a => State a -> Action a -> [[a]]
-sQuu :: Floating a => State a -> Action a -> [[a]]
-sQxu :: Floating a => State a -> Action a -> [[a]]
+sCx = cx sCost
+sCu = cu sCost
+sCxx = cxx sCost
+sCuu = cuu sCost
+sCxu = cxu sCost
 
+-- q fcn quadratic expansion
+sQx  :: Floating a => State a -> Action a -> Quad a -> [a]
+sQu  :: Floating a => State a -> Action a -> Quad a -> [a]
+sQxx :: Floating a => State a -> Action a -> Quad a -> [[a]]
+sQuu :: Floating a => State a -> Action a -> Quad a -> [[a]]
+sQxu :: Floating a => State a -> Action a -> Quad a -> [[a]]
 
-sQx = qx sCost
-sQu = qu sCost
-sQxx = qxx sCost
-sQuu = quu sCost
-sQxu = qxu sCost
+dtTestQ :: Floating a => a
+dtTestQ = 0.1
+sQx  = qx  sCost (\x u -> sEuler x u dtTestQ)
+sQu  = qu  sCost (\x u -> sEuler x u dtTestQ)
+sQxx = qxx sCost (\x u -> sEuler x u dtTestQ)
+sQuu = quu sCost (\x u -> sEuler x u dtTestQ)
+sQxu = qxu sCost (\x u -> sEuler x u dtTestQ)
