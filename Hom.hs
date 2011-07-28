64c6e7043bb6a71dbdca9a414ffcb6e7245dcdf2
Greg Horn <gregmainland@gmail.com>
2011-07-27 22:18:47 -0700

    fixed bugs in eulerStep and rk4Step

--- Hom.hs
+++ Hom.hs
@@ -15,18 +15,18 @@ type Ode a = State a -> Action a -> State a
 
 ---------------- general stuff -----------------
 eulerStep :: (Floating a) => Ode a -> State a -> Action a -> a -> State a
-eulerStep dxdt x u dt = map (*dt) (dxdt x u)
+eulerStep dxdt x u dt = zipWith (+) x $ map (*dt) (dxdt x u)
 
 rk4Step :: (Floating a) => Ode a -> State a -> Action a -> a -> State a
-rk4Step dxdt x u dt = map (/6) $ addLists [k1, twok2, twok3, k4]
+rk4Step dxdt x u dt = zipWith (+) x $ map (/6) $ addLists [k1, twok2, twok3, k4]
   where
     addLists [] = []
     addLists (a:as) = foldl (\acc y -> zipWith (+) acc y) a as
     
     k1 = map (*dt) $ dxdt x u
-    k2 = map (*dt) $ addLists [x, map (*0.5) k1]
-    k3 = map (*dt) $ addLists [x, map (*0.5) k2]
-    k4 = map (*dt) $ addLists [x, k3]
+    k2 = map (*dt) $ dxdt (addLists [x, map (*0.5) k1]) u
+    k3 = map (*dt) $ dxdt (addLists [x, map (*0.5) k2]) u
+    k4 = map (*dt) $ dxdt (addLists [x, k3])            u
     
     twok2 = map (*2) k2
     twok3 = map (*2) k3
