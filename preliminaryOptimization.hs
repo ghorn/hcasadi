c358a5e8d779f0b13cc8dd388951dc149d0c8c72
Greg Horn <gregmainland@gmail.com>
2011-08-17 22:17:15 -0700

    added basic bfgs with golden section line search

--- preliminaryOptimization.hs
+++ preliminaryOptimization.hs
@@ -61,11 +61,37 @@ oneDfp xk vk f g = (xkp1, vkp1)
     (alphakp1, _) = head $ drop 200 $ goldenSectionSearch f' (0,10)
     xkp1 = xk + (scale alphakp1 pk)
     
+bfgs :: (Product e, Container Vector e, Ord t, Num (Vector e), Floating e) =>
+       Vector e -> (Vector e -> t) -> (Vector e -> Vector e) -> [(Vector e, Matrix e)]
+bfgs x0 f g = iterate (\(x,v) -> oneBfgs x v f g) (x0, v0)
+  where
+    v0 = ident $ fromIntegral $ length (toList x0)
+
+oneBfgs :: (Product e, Container Vector e, Ord t, Num (Vector e), Floating e) =>
+       Vector e -> Matrix e -> (Vector e -> t) -> (Vector e -> Vector e) -> (Vector e, Matrix e)
+oneBfgs xk vk f g = (xkp1, vkp1)
+  where
+    eye = ident $ cols vk
+    den = 1/(sk <.> yk)
+    
+    vkp1 = (eye - (scale den (outer sk yk))) <> vk <> (eye - (scale den (outer yk sk)))
+           + (scale den (outer sk sk))
+    gk   = g xk
+    gkp1 = g xkp1
+    yk = gkp1 - gk
+    sk = xkp1 - xk
+    
+    pk = -( vk <> gk )
+    f' alpha = f $ xk + (scale alpha pk)
+    (alphakp1, _) = head $ drop 200 $ goldenSectionSearch f' (0,10)
+    xkp1 = xk + (scale alphakp1 pk)
+
     
 main :: IO ()
 main = do
   print "hi"
   let x0 = fromList [-1.2,1] :: Vector Double
-      results = (take 20 $ dfp x0 rosenbrockF rosenbrockG)
+--      results = (take 20 $ dfp x0 rosenbrockF rosenbrockG)
+      results = (take 20 $ bfgs x0 rosenbrockF rosenbrockG)
   mapM_ (\(_,v) -> print $ inv v) results
   mapM_ (\(x,_) -> print x) results
