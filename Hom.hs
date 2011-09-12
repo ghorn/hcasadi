4f4cc662686d9bd04951f24a07085ef0533184d2
Greg Horn <gregmainland@gmail.com>
2011-09-11 18:38:09 -0700

    restored dA/dB functions

--- Hom.hs
+++ Hom.hs
@@ -4,7 +4,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Hom (Ode, Cost,
---            dA, dB,
+            dA, dB,
             Quad(..), evalQuad) where
 
 import Casadi
@@ -14,18 +14,11 @@ type Ode a = a -> a -> a
 type Cost a b = a -> a -> b
 
 ---------- linearize dynamics f ~= f0 + dA*(x-x0) + dB*(u - u0) ---------
---dA :: Floating a => Ode a -> [[a]]
---dA f x u = jacobian g x
---  where
---    g x' = f x' (map lift u)
---
---
---dB :: Floating a => (forall s b. (Floating b, Mode s) =>
---                     [AD s b] -> [AD s b] -> [AD s b]) -> (State a -> Action a -> [[a]])
---dB f x u = jacobian g u
---  where
---    g u' = f (map lift x) u'
+dA :: Ode SXMatrix -> SXMatrix -> SXMatrix -> SXMatrix
+dA f x u = jacobian (f x u) x
 
+dB :: Ode SXMatrix -> SXMatrix -> SXMatrix -> SXMatrix
+dB f x u = jacobian (f x u) u
 
 ------------- quadratic data type -----------
 --                   hess    grad   const    x0
